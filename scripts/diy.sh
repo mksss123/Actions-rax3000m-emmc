cf11573165d220eb2e56661637452384fe18f718
mksss123 <164472205+mksss123@users.noreply.github.com>
2024-05-27 14:25:52 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 git clone https://github.com/liudf0716/luci-app-apfree-wifidog package/applications/luci-app-apfree-wifidog
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
