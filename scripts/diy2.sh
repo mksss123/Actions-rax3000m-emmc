0d97e001cc1012593338e338bed07b966aeaf92c
mksss123 <164472205+mksss123@users.noreply.github.com>
2024-05-28 01:08:08 +0800

    Update diy2.sh

--- scripts/diy2.sh
+++ scripts/diy2.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 echo "gitclone apfree-wifigod-luci"
-git clone https://github.com/liudf0716/luci-app-apfree-wifidog
+git clone https://github.com/liudf0716/luci-app-apfree-wifidog feeds/luci/applications/luci-app-apfree-wifidog/
 ls
 echo "mv to package "
-mv luci-app-apfree-wifidog feeds/luci/applications/luci-app-apfree-wifidog/
-./scripts/feeds install luci -a
-./scripts/feeds install luci -luci-app-apfree-wifidog
+./scripts/feeds install -a
 
 make menuconfig
 #更改默认地址为192.168.6.1
